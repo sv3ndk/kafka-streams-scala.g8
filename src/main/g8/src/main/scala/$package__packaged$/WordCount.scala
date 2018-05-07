362b2c46b7a606a5c7def22c6f59aa795468e2d0
sv3ndk <svend.vanderveken@gmail.com>
2018-05-07 14:37:29 +0200

    now uses Lightbend's default Serdes in demo app

--- src/main/g8/src/main/scala/$package__packaged$/WordCount.scala
+++ src/main/g8/src/main/scala/$package__packaged$/WordCount.scala
@@ -1,42 +1,66 @@
 package $organization$.$name$
 
-import java.util.Properties
+import java.util.{Calendar, Properties}
 
-import org.apache.kafka.common.serialization.{Serde, Serdes}
-import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.{Consumed, StreamsBuilder, StreamsConfig}
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 import com.lightbend.kafka.scala.streams.ImplicitConversions._
-import com.lightbend.kafka.scala.streams.KStreamS
-import org.apache.kafka.streams.kstream.{Produced, Serialized}
+import com.lightbend.kafka.scala.streams.{KStreamS, StreamsBuilderS}
+import com.lightbend.kafka.scala.streams.DefaultSerdes._
+import org.apache.kafka.streams.kstream.Printed
 
-object Hello extends App {
+/**
+  * Make sure to create 2 topics before running this:
+  *
+  *    kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic rawSentences
+  *
+  *    kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic  countedTokens
+  *
+  * Then add some content in the first one:
+  *
+  *   kafka-console-producer --broker-list localhost:9092  --topic rawSentences
+  *
+  * which you can check by just tailing that topic:
+  *
+  *   kafka-console-consumer --bootstrap-server localhost:9092 --topic rawSentences --from-beginning
+  *
+  * One the programme below runs (e.g. through "sbt run"), you can simply inspect its output with:
+  *
+  *   kafka-console-consumer --bootstrap-server localhost:9092 --key-deserializer org.apache.kafka.common.serialization.StringDeserializer --value-deserializer org.apache.kafka.common.serialization.LongDeserializer --from-beginning --topic countedTokens
+  *
+  *
+  * */
 
-  val stringSerde = Serdes.String()
-  val longSerde = Serdes.Long().asInstanceOf[Serde[Long]]
+object WordCount extends App {
 
   val config: Properties = {
     val p = new Properties()
-    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application")
+    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application" + Calendar.getInstance.getTimeInMillis)
     p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-    p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-    p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+
+    // helps for debug (not optimal for prod): makes KTable data immediately available, without de-duplications
+    p.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0)
     p
   }
-  val builder: StreamsBuilder = new StreamsBuilder()
-  val textLines: KStreamS[String, String] = builder.stream("TextLinesTopic", Consumed.`with`(stringSerde, stringSerde))
 
-  textLines
-    .flatMapValues( line => line.split("\\\\W+").toIterable )
+  val builder = new StreamsBuilderS()
+  val textLines = builder.stream[String, String]("rawSentences")
+
+  val counted: KStreamS[String, Long] = textLines
+    .flatMapValues( line => line.split("\\W+").toIterable )
     .map( (_, w) => (w, 1l))
-    .groupByKey(Serialized.`with`(stringSerde, longSerde))
-    .count()
+    .groupByKey
+    .count
     .toStream
-    .to("wordsCountTopic", Produced.`with`(stringSerde, longSerde))
 
+  // print to console
+  counted.print(Printed.toSysOut[String, Long])
+
+
+  // serialize to topic, with default Serdes
+  counted.to("countedTokens")
 
   val streams: KafkaStreams = new KafkaStreams(builder.build, config)
-  streams.start
-
+  streams.start()
 
 }
 
